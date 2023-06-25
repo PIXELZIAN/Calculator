0cc8474927e1dee425d063c0781af8d7f50bf9cd
CianDickerHughes <129450952+PIXELZIAN@users.noreply.github.com>
2023-06-25 19:18:35 +0100

    updated styles and added JavaScript

---
+++ js/indexJs.js
@@ -0,0 +1,75 @@
+function AC(){
+    alert("Text have been all clear")
+}
+
+function Bracket(){
+    alert("Clear all text")
+}
+
+function Percent(){
+    alert("Clear all text")
+}
+
+function Division(){
+    alert("Clear all text")
+}
+
+function seven(){
+    alert("Clear all text")
+}
+
+function eight(){
+    alert("Clear all text")
+}
+
+function nine(){
+    alert("Clear all text")
+}
+
+function Multiplication(){
+    alert("Clear all text")
+}
+
+function four(){
+    alert("Clear all text")
+}
+
+function five(){
+    alert("Clear all text")
+}
+
+function six(){
+    alert("Clear all text")
+}
+
+function minus(){
+    alert("Clear all text")
+}
+
+function one(){
+    alert("Clear all text")
+}
+
+function two(){
+    alert("Clear all text")
+}
+
+function three(){
+    alert("Clear all text")
+}
+
+function plus(){
+    alert("Clear all text")
+}
+
+function zero(){
+    alert("Clear all text")
+}
+
+function decimal(){
+    alert("Clear all text")
+}
+
+function equal(){
+    alert("Clear all text")
+}
